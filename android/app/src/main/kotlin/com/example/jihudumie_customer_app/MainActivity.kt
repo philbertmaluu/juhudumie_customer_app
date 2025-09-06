3f42cf8feaec4fe00b4a0680d7df53d6cce6b69e
Philbert Malulu <philbertmaluu96@gmail.com>
2025-09-06 06:57:08 +0300

    iniitial commit

---
+++ android/app/src/main/kotlin/com/example/jihudumie_customer_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jihudumie_customer_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Scaffold(
+    body: Center(
+      child: Text('Hello, World!'),
+    ),
+  ));
+}
+
+
+
